b72f7cd5d3004b81d18dde30f8194ec76913af72
MatiasTanoni <matitanoni@gmail.com>
2025-11-07 18:55:18 -0300

    usuario registrado guardado correctamente

--- Front/src/app/service/auth.ts
+++ Front/src/app/service/auth.ts
@@ -12,22 +12,20 @@ export class Auth {
   private apiUrl = environments.apiUrl + '/auth';
   user = signal<any | boolean>(false);
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
-  async register(formData: FormData): Promise<{ success: boolean, message: string }> {
-    console.log('AUTH SERVICE REGISTER', formData);
+  async register(formData: FormData): Promise<{ success: boolean; message: string }> {
     try {
       const response = await firstValueFrom(
-        this.http.post<{ success: boolean, message: string }>(`${this.apiUrl}/register`, formData)
+        this.http.post<{ success: boolean; message: string; user?: any }>(`${this.apiUrl}/register`, formData)
       );
-      if (response.success) {
-        this.user.set(true);
+
+      if (response.success && response.user) {
+        this.user.set(response.user); // ✅ guardamos el usuario
         console.log('USUARIO REGISTRADO:', this.user());
       }
-      console.log('RESPUESTA DEL BACK:', response);
+
+      console.log('USUARIO REGISTRADO:', this.user());
       return response;
     } catch (error) {
       this.user.set(false);
@@ -36,17 +34,12 @@ export class Auth {
     }
   }
 
-  getUser(): any | boolean {
-    return this.user();
-  }
-
   async login(usernameOrEmail: string, password: string): Promise<{ success: boolean; message: string }> {
     try {
       const response = this.http.post<any>(`${this.apiUrl}/login`, { usernameOrEmail, password });
       const user = await firstValueFrom(response);
-      // Podés guardar el user si querés, pero sin navegar todavía
-      // this.user.set(user);
-      this.user.set(user);
+
+      this.user.set(user); // ✅ guardamos el objeto usuario del backend
       console.log('USUARIO LOGUEADO:', this.user());
       return { success: true, message: 'Login exitoso' };
     } catch (error: any) {
@@ -60,8 +53,11 @@ export class Auth {
   logout(): Promise<{ success: boolean; message: string }> {
     localStorage.clear();
     this.user.set(false);
-    // this.currentUser.set(null);
     this.router.navigate(['/auth']);
     return Promise.resolve({ success: true, message: 'Sesión cerrada exitosamente' });
+  }
+
+  getUser(): any | boolean {
+    return this.user();
   }
 }
